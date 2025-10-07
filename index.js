941cefbdb52c37f9845cb681dee97f137de93716
Chris <148207385+Xencil@users.noreply.github.com>
2025-10-07 13:50:47 +0100

    Updated message and added readme

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require("http"); 
+var http = require("http")
 const port = 8000; 
 
 http.createServer(function(req, res) { 
@@ -11,12 +11,12 @@ http.createServer(function(req, res) {
         <body>
             <h1>Welcome to My Page</h1>
             <h2>About me:</h2>
-            <p>Hi! My name is Chris. I am currently undertaking the Dynmaic web applications module where i am learning how to build simple node.js apps.</p>
+            <p>Hi! My name is Chris. I am currently undertaking the dynamic web applications module where i am learning how to build simple node.js apps.</p>
             <p>I enjoy working out, gaming and reading books.</p>
         </body>
         </html>`
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(Details);
     }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
+        console.log(`Node server is running on port ${port}...`)
 }); 
